c4c78884b53231cc0d4b1f0ec1688d79bdcc7015
ClementS03 <clement.seguin63@gmail.com>
2020-08-31 14:57:00 +0200

    fictional-university theme first commit

---
+++ js/scripts.js
@@ -0,0 +1,19 @@
+import "../css/style.css"
+
+// Our modules / classes
+import MobileMenu from "./modules/MobileMenu"
+import HeroSlider from "./modules/HeroSlider"
+import GMap from "./modules/GoogleMap"
+import Search from "./modules/Search"
+
+
+// Instantiate a new object using our modules/classes
+const mobileMenu = new MobileMenu()
+const heroSlider = new HeroSlider()
+const googleMap = new GMap()
+const search = new Search() 
+
+// Allow new JS and CSS to load in browser without a traditional page refresh
+if (module.hot) {
+  module.hot.accept()
+}
